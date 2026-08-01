7712b6fc6931ffb39b5b1b9ffcb4aec3b3c90460
mfjaconis <matheusjaconis@gmail.com>
2026-08-01 11:46:10 -0300

    add multi-module Maven project structure

---
+++ account-service/src/main/java/com/jaconis/Main.java
@@ -0,0 +1,8 @@
+package com.jaconis;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello account-service");
+    }
+}
---
+++ api-gateway/src/main/java/com/jaconis/Main.java
@@ -0,0 +1,9 @@
+package com.jaconis;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello api-gateway");
+
+    }
+}
